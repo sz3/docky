ea099222dc3d87ffbbdbaaa6cdef183536af6e4a
Jason Smith <jason@t500>
2009-09-03 03:46:10 -0400

    missing file

---
+++ Docky/Docky/Items/SeparatorItem.cs
@@ -0,0 +1,60 @@
+//  
+//  Copyright (C) 2009 Jason Smith
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Cairo;
+using Gdk;
+using Gtk;
+
+using Docky.CairoHelper;
+
+namespace Docky.Items
+{
+
+
+	public class SeparatorItem : AbstractDockItem
+	{
+		public override bool RotateWidthDock {
+			get { return true; }
+		}
+		
+		public override bool Square {
+			get {
+				return false;
+			}
+		}
+		
+		public SeparatorItem ()
+		{
+		}
+		
+		protected override DockySurface CreateIconBuffer (Surface model, int size)
+		{
+			return new DockySurface ((int) (size * .2), size, model);
+		}
+		
+		protected override void PaintIconSurface (DockySurface surface)
+		{
+			// fixme
+		}
+	}
+}
