c1da4e2f60d03be6bd1377e46d57fa6225c7aeb8
Jason Smith <jason@t500>
2009-09-03 01:09:05 -0400

    missing files

---
+++ Docky.Items/Docky.Items/ApplicationDockItem.cs
@@ -0,0 +1,67 @@
+//  
+//  Copyright (C) 2009 Jason Smith
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Cairo;
+using Gdk;
+using Gtk;
+using Wnck;
+
+namespace Docky.Items
+{
+
+	public class ApplicationDockItem : WnckDockItem
+	{
+		public static ApplicationDockItem NewFromFilename (string filename)
+		{
+			Gnome.DesktopItem desktopItem;
+			
+			try {
+				desktopItem = Gnome.DesktopItem.NewFromFile (filename, 0);
+			} catch (Exception e) {
+				Console.Error.WriteLine (e.Message);
+				return null;
+			}
+			
+			if (desktopItem == null)
+				return null;
+			
+			return new ApplicationDockItem (desktopItem);
+		}
+		
+		Gnome.DesktopItem desktop_item;
+		
+		public override IEnumerable<Wnck.Window> Windows {
+			get {
+				yield break;
+			}
+		}
+		
+		private ApplicationDockItem (Gnome.DesktopItem item)
+		{
+			desktop_item = item;
+			if (item.AttrExists ("Icon"))
+				Icon = item.GetString ("Icon");
+		}
+
+	}
+}
---
+++ Docky.Items/Docky.Items/IconDockItem.cs
@@ -0,0 +1,81 @@
+//  
+//  Copyright (C) 2009 Jason Smith
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Cairo;
+using Gdk;
+using Gtk;
+
+using Docky.CairoHelper;
+using Docky.Services;
+
+namespace Docky.Items
+{
+
+
+	public abstract class IconDockItem : AbstractDockItem
+	{
+		string icon;
+		protected string Icon {
+			get { return icon; }
+			set {
+				if (icon == value)
+					return;
+				icon = value;
+				ResetBuffers ();
+			}
+		}
+		
+		public IconDockItem ()
+		{
+			icon = "default";
+		}
+		
+		protected override sealed void PaintIconSurface (DockySurface surface)
+		{
+			int iconSize = Math.Min (surface.Width, surface.Height);
+			
+			Gdk.Pixbuf pbuf = DockServices.Drawing.LoadIcon (Icon, iconSize);
+			
+			if (pbuf.Width != iconSize && pbuf.Height != iconSize) {
+				double scale = iconSize / (double) Math.Max (pbuf.Width, pbuf.Height);
+				
+				Gdk.Pixbuf temp = pbuf;
+				pbuf = temp.ScaleSimple ((int) (temp.Width * scale), 
+				                         (int) (temp.Height * scale), 
+				                                InterpType.Hyper);
+				
+				temp.Dispose ();
+			}
+			
+			using (Cairo.Context cr = surface.GetContext ()) {
+				Gdk.CairoHelper.SetSourcePixbuf (cr, 
+				                                 pbuf, 
+				                                 (surface.Width - pbuf.Width) / 2, 
+				                                 (surface.Height - pbuf.Height) / 2);
+				cr.Paint ();
+			}
+			
+			pbuf.Dispose ();
+		}
+	}
+}
