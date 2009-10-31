530e9389bff09f99de451db35dc7421474853ab8
Robert Dyer <psybers@gmail.com>
2009-10-31 00:55:23 -0500

    add ability to set docky autostart

--- Docky/Docky/ConfigurationWindow.cs
+++ Docky/Docky/ConfigurationWindow.cs
@@ -18,14 +18,17 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 using Cairo;
 using Gdk;
+using Gnome;
 using Gtk;
 
 using Docky.Interface;
+using Docky.Services;
 
 namespace Docky
 {
@@ -48,6 +51,9 @@ namespace Docky
 				}
 				i++;
 			}
+			
+			checkbutton1.Active = IsAutoStartEnabled ();
+			checkbutton1.Toggled += OnCheckbutton1Toggled;
 			
 			placement = new DockPlacementWidget (Docky.Controller.Docks);
 			placement.ActiveDockChanged += PlacementActiveDockChanged;
@@ -104,6 +110,85 @@ namespace Docky
 		protected virtual void OnThemeComboChanged (object sender, System.EventArgs e)
 		{
 			Docky.Controller.DockTheme = theme_combo.ActiveText;
+		}
+		
+		protected virtual void OnCheckbutton1Toggled (object sender, System.EventArgs e)
+		{
+			SetAutoStartEnabled (checkbutton1.Active);
+		}
+		
+		string AutoStartKey = "Hidden";
+		DesktopItem autostartfile;
+		
+		string AutoStartDir {
+			get {
+				return System.IO.Path.Combine (
+					Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData), "autostart");
+		    }
+		}
+		
+		string AutoStartFileName {
+		  get {
+		      return System.IO.Path.Combine (AutoStartDir, "docky.desktop");
+		    }
+		}
+		
+		string AutoStartUri {
+			get {
+				return Gnome.Vfs.Uri.GetUriFromLocalPath (AutoStartFileName);
+			}
+		}
+		
+		DesktopItem AutoStartFile {
+			get {
+				if (autostartfile != null) 
+					return autostartfile;
+				
+				try {
+					autostartfile = DesktopItem.NewFromUri (AutoStartUri, DesktopItemLoadFlags.NoTranslations);
+				} catch (GLib.GException loadException) {
+					Log<DockPlacementWidget>.Info ("Unable to load existing autostart file: {0}", loadException.Message);
+					Log<DockPlacementWidget>.Info ("Writing new autostart file to {0}", AutoStartFileName);
+					autostartfile = DesktopItem.NewFromFile (System.IO.Path.Combine (AssemblyInfo.InstallData, "applications/docky.desktop"),
+					                                         DesktopItemLoadFlags.NoTranslations);
+					try {
+						if (!Directory.Exists (AutoStartDir))
+							Directory.CreateDirectory (AutoStartDir);
+
+						autostartfile.Save (AutoStartUri, true);
+						autostartfile.Location = AutoStartUri;
+					} catch (Exception e) {
+						Log<DockPlacementWidget>.Error ("Failed to write initial autostart file: {0}", e.Message);
+					}
+				}
+				return autostartfile;
+			}
+		}
+		
+		bool IsAutoStartEnabled ()
+		{
+			DesktopItem autostart = AutoStartFile;
+			
+			if (!autostart.Exists ()) {
+				Log<SystemService>.Error ("Could not open autostart file {0}", AutoStartUri);
+			}
+			
+			if (autostart.AttrExists (AutoStartKey)) {
+				return !String.Equals(autostart.GetString (AutoStartKey), "true", StringComparison.OrdinalIgnoreCase);
+			}
+			return false;
+		}
+		
+		void SetAutoStartEnabled (bool enabled)
+		{
+			DesktopItem autostart = AutoStartFile;
+			
+			autostart.SetBoolean (AutoStartKey, !enabled);
+			try {
+				autostart.Save (null, true);
+			} catch (Exception e) {
+				Log<SystemService>.Error ("Failed to update autostart file: {0}", e.Message);
+			}
 		}
 	}
 }
