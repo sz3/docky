4f940358f82ca49539fd1498d98759355e0c0184
Jason Smith <jason@t500>
2009-11-18 00:57:04 -0600

    Prefer a single svg when available

--- Docky.Items/Docky.Items/ApplicationDockItem.cs
+++ Docky.Items/Docky.Items/ApplicationDockItem.cs
@@ -73,8 +73,14 @@ namespace Docky.Items
 		{
 			OwnedItem = item;
 			
-			if (item.HasAttribute ("Icon"))
+			if (item.HasAttribute ("Icon")) {
 				Icon = item.GetString ("Icon");
+				Gtk.IconInfo info = Gtk.IconTheme.Default.LookupIcon (Icon, 48, Gtk.IconLookupFlags.ForceSvg);
+				if (info.Filename.EndsWith (".svg")) {
+					Icon = info.Filename;
+				}
+				info.Dispose ();
+			}
 			
 			if (item.HasAttribute ("Name")) {
 				HoverText = item.GetLocaleString ("Name");
