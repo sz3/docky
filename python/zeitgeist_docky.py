779348de88e0b05aaec9daf4114145eb1f4b592e
Jason Smith <jason.smith@canonical.com>
2009-12-10 04:11:24 -0500

    Add zeitgeist plugin (0.3, not 0.2.1)

---
+++ python/zeitgeist_docky.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import gobject
+import time
+import glib
+import dbus
+import dbus.glib
+import sys
+import os
+
+from zeitgeist.client import ZeitgeistClient
+from zeitgeist.datamodel import Event, Subject, Interpretation, Manifestation
+
+dockypath  = '/org/gnome/Docky'
+dockybus   = 'org.gnome.Docky'
+dockyiface = 'org.gnome.Docky'
+itemiface  = 'org.gnome.Docky.Item'
+
+try:
+	CLIENT = ZeitgeistClient()
+except RuntimeError, e:
+	print "Unable to connect to Zeitgeist, won't send events. Reason: '%s'" %e
+	CLIENT = None
+
+class MostUsedProvider():
+	def __init__(self):
+		self._zg = CLIENT
+
+	def get_directory_most_used( self, directory, handler):
+		today = time.time()
+		delta = today - 14 * 86400
+
+		def _handle_find_events(ids):
+			self._zg.get_events(ids, _handle_get_events)
+
+		def _handle_get_events(events):
+			uris = []
+			for event in events:
+				for subject in event.subjects:
+					uris.append(subject)
+			handler(directory, uris)
+
+		event = Event()
+		subject = Subject()
+		subject.set_origin(directory)
+		event.set_subjects([subject])
+		self._zg.find_event_ids_for_templates([event],_handle_find_events, [delta*1000, today*1000], num_events=5, result_type=5)
+
+class DockyUriItem():
+	def __init__(self, path):
+		self.path = path
+		self.bus = dbus.SessionBus ()
+		
+		self.id_map = {}
+		
+		obj = self.bus.get_object (dockybus, self.path)
+		self.iface = dbus.Interface (obj, itemiface)
+	
+		self.bus.add_signal_receiver (self.menu_pressed_signal, "MenuItemActivated", itemiface, dockybus, self.path)
+	
+		self.mostusedprovider = MostUsedProvider ()
+		
+		self.update_most_used ()
+		self.timer = glib.timeout_add (2 * 60 * 1000, self.handle_timeout)
+		self.update_timer = glib.timeout_add (20 * 60 * 1000, self.handle_update_timeout)
+	
+	
+	def dispose(self):
+		for k, v in self.id_map.iteritems():
+			self.iface.RemoveItem (k)
+		glib.source_remove (self.timer)
+		glib.source_remove (self.update_timer)
+	
+	def handle_timeout(self):
+		for k, v in self.id_map.iteritems():
+			self.iface.ConfirmItem (k)
+		return True
+		
+	def handle_update_timeout(self):
+		for menu_id, uri in self.id_map.iteritems():
+			self.iface.RemoveItem (k)
+		update_most_used ()
+		return True
+	
+	def update_most_used(self):
+		uri = self.iface.GetUri ();
+		self.mostusedprovider.get_directory_most_used (uri, self._handle_get_most_used)
+
+	def _handle_get_most_used(self, directory, uris):
+		for subject in uris:
+			menu_id = self.iface.AddMenuItem (subject.text, "gtk-file", "Recent Items")
+			self.id_map[menu_id] = subject.uri
+		
+	def menu_pressed_signal(self, menu_id):
+		os.spawnlp(os.P_NOWAIT, "gnome-open", "gnome-open", self.id_map[menu_id])
+
+class DockySink():
+	def __init__(self):
+		self.bus = dbus.SessionBus ()
+		self.items = {}
+
+		obj = self.bus.get_object (dockybus, dockypath)
+		self._iface = dbus.Interface (obj, dockyiface)
+
+		paths = self._iface.DockItemPaths()
+		
+		self.bus.add_signal_receiver (self.item_added, "ItemAdded", dockyiface, dockybus, dockypath)
+		self.bus.add_signal_receiver (self.item_removed, "ItemRemoved", dockyiface, dockybus, dockypath)
+		
+		for pathtoitem in paths:
+			obj = self.bus.get_object (dockybus, pathtoitem)
+			item = dbus.Interface (obj, itemiface)
+			if item.GetOwnsUri():
+				self.items[pathtoitem] = DockyUriItem (pathtoitem)
+	
+	def item_added(self, path):
+		obj = self.bus.get_object (dockybus, path)
+		item = dbus.Interface (obj, itemiface)
+		if item.GetOwnsUri():
+			self.items[path] = DockyUriItem (path)
+
+	def item_removed(self, path):
+		if path in self.items:
+			self.items[path].dispose ()
+			del self.items[path]
+	
+	def dispose(self):
+		for path, item in self.items.iteritems ():
+			item.dispose ()
+			
+	
+if __name__ == "__main__":
+	dockysink = DockySink()
+	mainloop = gobject.MainLoop(is_running=True)
+
+	while mainloop.is_running():
+		print 'running'
+		try:
+		    mainloop.run()
+		except KeyboardInterrupt:
+		    dockysink.dispose ()
+		    gobject.idle_add(quit, 1)
+		print 'done\n\n'
