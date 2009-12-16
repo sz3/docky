fc22c273dda5c946d687f69e61c1de98dfa17e59
Jason Smith <jason.smith@canonical.com>
2009-12-16 14:51:46 -0500

    More general catching

--- Docky.DBus/Docky.DBus/DBusManager.cs
+++ Docky.DBus/Docky.DBus/DBusManager.cs
@@ -110,7 +110,7 @@ namespace Docky.DBus
 			
 			try {
 				Bus.Session.Unregister (path);
-			} catch (System.Reflection.TargetInvocationException e) {
+			} catch {
 				Log<DBusManager>.Error ("Could not unregister: " + path);
 				Log<DBusManager>.Debug (e.StackTrace);
 				return;
