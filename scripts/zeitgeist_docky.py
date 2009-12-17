f57d06f8a9b693bd90d8314053423a12b64f6bfc
Seif Lotfy <seif@lotfy.com>
2009-12-17 05:22:13 +0100

    added the journal and fixed minor bugs TODO: add zeitgeist_journal to makefile

--- scripts/zeitgeist_docky.py
+++ scripts/zeitgeist_docky.py
@@ -4,10 +4,12 @@ import gobject
 import time
 import glib
 import dbus
+import gtk
 import dbus.glib
 import sys
 import urllib
 import os
+from zeitgeist_journal import Window
 
 try:
 	from docky.docky import DockyItem, DockySink
@@ -42,10 +44,10 @@ class MostUsedProvider():
 			for event in events:
 				if counter < 5:
 					for subject in event.subjects:
-						if counter < 7 and exists(subject.uri):
+						if counter < 5 and exists(subject.uri):
 							uris.append(subject)
 							counter+=1
-						elif counter >= 7:
+						elif counter >= 5:
 							break
 						else:
 							pass
@@ -59,10 +61,10 @@ class MostUsedProvider():
 			subject = Subject()
 			subject.set_origin(path)
 			event.set_subjects([subject])
-			self._zg.find_event_ids_for_templates([event],_handle_find_events, [delta, today], StorageState.Any, 0, 5) 
+			self._zg.find_event_ids_for_templates([event],_handle_find_events, [delta, today], StorageState.Any, 0, 4) 
 		else:
 			event.set_actor(path)
-			self._zg.find_event_ids_for_templates([event],_handle_find_events, [delta, today], StorageState.Any, 0, 5) 
+			self._zg.find_event_ids_for_templates([event],_handle_find_events, [delta, today], StorageState.Any, 0, 4) 
 
 class DockyZGItem(DockyItem):
 	def __init__(self, path):
@@ -71,25 +73,32 @@ class DockyZGItem(DockyItem):
 		self.update_most_used()
 	
 	def update_most_used(self):
-		uri = ""
+		self.uri = ""
 		if self.iface.GetOwnsUri():
-			uri = self.iface.GetUri();
+			self.uri = self.iface.GetUri();
 		elif self.iface.GetOwnsDesktopFile():
-			uri = self.iface.GetDesktopFile()
+			self.uri = self.iface.GetDesktopFile()
 		else:
 			return
-		
-		self.mostusedprovider.get_path_most_used (uri, self._handle_get_most_used, self.iface.GetOwnsUri ())
+		self.mostusedprovider.get_path_most_used (self.uri, self._handle_get_most_used, self.iface.GetOwnsUri ())
 
 	def _handle_get_most_used(self, uris):
 		for subject in uris:
 			menu_id = self.iface.AddFileMenuItem(subject.uri, "Most Used Items")
 			self.id_map[menu_id] = subject.uri
+		if len(uris) > 0:
+			menu_id = self.iface.AddMenuItem("Journal", "", "")
+			self.id_map[menu_id] = "Journal"
+
+	def menu_pressed(self, menu_id):
+		window = Window(CLIENT)
+		window.load_events(0, time.time(), self.uri)
 
 class DockyZGSink(DockySink):
 	def item_path_found(self, pathtoitem, item):
 		if item.GetOwnsUri() or item.GetOwnsDesktopFile():
 			self.items[pathtoitem] = DockyZGItem(pathtoitem)
+
 
 if __name__ == "__main__":
 	dockysink = DockyZGSink()
