6db5888497cae61a0371ab71aeda4690f85d2c34
Robert Dyer <psybers@gmail.com>
2010-06-02 21:43:17 -0500

    support XDG Base Directory specification - bug 534178

--- Docky.Services/Docky.Services/PathsService.cs
+++ Docky.Services/Docky.Services/PathsService.cs
@@ -1,5 +1,5 @@
 //  
-//  Copyright (C) 2010 Chris Szikszoy
+//  Copyright (C) 2010 Chris Szikszoy, Robert Dyer
 // 
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -22,45 +22,70 @@ namespace Docky.Services
 {
 	public class PathsService
 	{
-		static readonly File home_folder = FileFactory.NewForPath (Environment.GetEnvironmentVariable ("HOME"));
+		public File SystemDataFolder { get; protected set; }
 		
-		public File SystemDataFolder {
-			get { return FileFactory.NewForPath (AssemblyInfo.DataDirectory).GetChild ("docky"); }
-		}
-
-		public File DockManagerUserDataFolder {
-			get { return FileFactory.NewForPath (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData)).GetChild ("dockmanager"); }
-		}
-
-		public File UserDataFolder {
-			get { return FileFactory.NewForPath (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData)).GetChild ("docky"); }
+		public File DockManagerUserDataFolder { get; protected set; }
+		
+		public File UserDataFolder { get; protected set; }
+		
+		public File UserCacheFolder { get; protected set; }
+		
+		public File AutoStartFile { get; protected set; }
+		
+		public PathsService ()
+		{
+			// get environment-based settings
+			File env_home         = FileFactory.NewForPath (Environment.GetEnvironmentVariable ("HOME"));
+			File env_data_home    = FileFactory.NewForPath (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData));
+			File env_data         = FileFactory.NewForPath (Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData));
+			File env_data_install = FileFactory.NewForPath (AssemblyInfo.DataDirectory);
+			
+			
+			// get XDG Base Directory settings
+			string xdg_data_home  = Environment.GetEnvironmentVariable ("XDG_DATA_HOME");
+			string xdg_data_dirs  = Environment.GetEnvironmentVariable ("XDG_DATA_DIRS");
+			string xdg_cache_home = Environment.GetEnvironmentVariable ("XDG_CACHE_HOME");
+			
+			
+			// determine directories based on XDG with fallbacks
+			File cache_folder;
+			if (!string.IsNullOrEmpty (xdg_cache_home))
+				cache_folder = FileFactory.NewForPath (xdg_cache_home);
+			else
+				cache_folder = env_home.GetChild (".cache");
+			
+			File data_folder;
+			if (!string.IsNullOrEmpty (xdg_data_home))
+				data_folder = FileFactory.NewForPath (xdg_data_home);
+			else
+				data_folder = env_data_home;
+			
+			
+			// set the XDG Base Directory specified directories to use
+			UserCacheFolder           = cache_folder.GetChild ("docky");
+			DockManagerUserDataFolder = data_folder.GetChild ("dockmanager");
+			UserDataFolder            = data_folder.GetChild ("docky");
+			
+			
+			// set the non-XDG Base Directory specified directories to use
+			SystemDataFolder = env_data_install.GetChild ("docky");
+			AutoStartFile    = env_data.GetChild ("autostart").GetChild ("docky.desktop");
+			
+			
+			// ensure all writable directories exist
+			EnsureDirectoryExists (UserCacheFolder);
+			EnsureDirectoryExists (DockManagerUserDataFolder);
+			EnsureDirectoryExists (UserDataFolder);
 		}
 		
-		File user_cache_folder;
-		public File UserCacheFolder {
-			get { 
-				if (user_cache_folder != null)  
-					return user_cache_folder;
-				
-				string xdg_cache_home = Environment.GetEnvironmentVariable ("XDG_CACHE_HOME");
-				if (!string.IsNullOrEmpty (xdg_cache_home))
-					user_cache_folder = FileFactory.NewForPath (xdg_cache_home).GetChild ("docky");
-				else
-					user_cache_folder = home_folder.GetChild (".cache").GetChild ("docky");
-				
-				if (!user_cache_folder.Exists)
-					try {
-						user_cache_folder.MakeDirectoryWithParents (null);
-					} catch {
-						Log<PathsService>.Fatal ("Could not access the cache directory '" + user_cache_folder.Path + "' or create it.  Docky will not work properly unless this folder is writable.");
-					}
-				
-				return user_cache_folder;
-			}
-		}
-
-		public File AutoStartFile {
-			get { return FileFactory.NewForPath (Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData)).GetChild ("autostart").GetChild ("docky.desktop"); }
+		static void EnsureDirectoryExists (File dir)
+		{
+			if (!dir.Exists)
+				try {
+					dir.MakeDirectoryWithParents (null);
+				} catch {
+					Log<PathsService>.Fatal ("Could not access the directory '" + dir.Path + "' or create it.  Docky will not work properly unless this folder is writable.");
+				}
 		}
 	}
 }
